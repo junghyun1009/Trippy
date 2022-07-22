4da3ed6f56882bdf830136ee80aa13071ab3856b
junghyun1009 <junghyunk1009@gmail.com>
2022-07-22 23:31:59 +0900

    [#S07P12A506-63][FE] refactor: add diary form

---
+++ front/src/components/diary/DiaryForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div>
+      <el-tag
+        v-for="tag in tags"
+        :key="tag.name"
+        class="mx-1"
+        closable
+        :type="tag.type"
+      >
+        {{ tag.name }}
+      </el-tag>
+    </div>
+    <!-- 옵션:여행 기간 -->
+    <div class="date-picker">
+      <div class="block">
+        <span class="demonstration">여행 기간</span>
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          range-separator="To"
+          start-placeholder="Start date"
+          end-placeholder="End date"
+          :size="size"
+        />
+      </div>
+    </div>
+
+    <!-- 옵션: 일행 타입 -->
+    <div class="party-type">
+      <span class="demonstration">일행 타입</span>
+      <el-radio-group v-model="radio">
+        <el-radio :label="3">가족</el-radio>
+        <el-radio :label="6">커플</el-radio>
+        <el-radio :label="9">친구</el-radio>
+        <el-radio :label="12">개인</el-radio>
+      </el-radio-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'DiaryForm',
+  data() {
+    return {
+      value1: ref(''),
+      radio: ref(3)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.date-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
+.date-picker .block {
+  padding: 30px 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color);
+  flex: 1;
+}
+.date-picker .block:last-child {
+  border-right: none;
+}
+.date-picker .demonstration {
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-right: 20px;
+}
+.party-type .demonstration {
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-right: 20px;
+}
+</style>
--- front/src/views/diary/DiaryCreateView.vue
+++ front/src/views/diary/DiaryCreateView.vue
@@ -1,53 +1,22 @@
 <template>
   <div>
     <h1>Diary Create</h1>
-    <div class="demo-date-picker">
-      <div class="block">
-        <span class="demonstration">Default</span>
-        <el-date-picker
-          v-model="value1"
-          type="date"
-          placeholder="Pick a day"
-        />
-      </div>
-    </div>
+    <diary-form></diary-form>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import DiaryForm from "@/components/diary/DiaryForm.vue"
 
 export default {
   name: 'DiaryCreateView',
-  data() {
-    return {
-      value1: ref('')
-    }
+  components: {
+    DiaryForm,
   }
 }
 
 </script>
 
 <style scoped>
-.demo-date-picker {
-  display: flex;
-  width: 100%;
-  padding: 0;
-  flex-wrap: wrap;
-}
-.demo-date-picker .block {
-  padding: 30px 0;
-  text-align: center;
-  border-right: solid 1px var(--el-border-color);
-  flex: 1;
-}
-.demo-date-picker .block:last-child {
-  border-right: none;
-}
-.demo-date-picker .demonstration {
-  display: block;
-  color: var(--el-text-color-secondary);
-  font-size: 14px;
-  margin-bottom: 20px;
-}
+
 </style>
