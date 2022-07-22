ba8660a7dadc32f0c24ccac51f69571c10715333
junghyun1009 <junghyunk1009@gmail.com>
2022-07-23 01:13:52 +0900

    [#S07P12A506-63][FE] feat: add num and transport

--- front/src/components/diary/DiaryForm.vue
+++ front/src/components/diary/DiaryForm.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
+    <!-- 태그 -->
     <div>
-      <el-tag
-        class="mx-1"
-        closable
-        type=''
-      >
-        {{ optionTag }}
+      <el-tag class="mx-1" type=''>
+        {{ partyTag }}
+      </el-tag>
+      <el-tag v-for="trans in transportationTag" :key="trans" class="mx-1" 
+      closable :disable-transitions="false" type='' @close="handleClose(trans)">
+        {{ trans }}
       </el-tag>
     </div>
+
     <!-- 옵션:여행 기간 -->
     <div class="date-picker">
       <div class="block">
         <span class="demonstration">여행 기간</span>
         <el-date-picker
-          v-model="value1"
+          v-model="datePick"
           type="daterange"
           range-separator="To"
           start-placeholder="Start date"
           end-placeholder="End date"
-          :size="size"
         />
       </div>
     </div>
@@ -27,12 +28,29 @@
     <!-- 옵션: 일행 타입 -->
     <div class="party-type">
       <span class="demonstration">일행 타입</span>
-      <el-radio-group v-model="radio">
+      <el-radio-group v-model="partyType">
         <el-radio label="가족">가족</el-radio>
         <el-radio label="커플">커플</el-radio>
         <el-radio label="친구">친구</el-radio>
         <el-radio label="개인">개인</el-radio>
       </el-radio-group>
+    </div>
+
+    <!-- 옵션: 인원 수 -->
+    <div class="member-num">
+      <span class="demonstration">인원 수</span>
+      <el-input-number v-model="num" :min="1" :max="10"/>
+    </div>
+
+  <!-- 옵션: 이동수단 -->
+    <div>
+      <el-checkbox-group v-model="transportationList">
+        <el-checkbox label="뚜벅이" />
+        <el-checkbox label="대중교통" />
+        <el-checkbox label="따릉이" />
+        <el-checkbox label="택시" />
+        <el-checkbox label="자차" />
+      </el-checkbox-group>
     </div>
   </div>
 </template>
@@ -43,14 +61,25 @@ export default {
   name: 'DiaryForm',
   data() {
     return {
-      value1: '',
-      radio: '가족',
+      datePick: '',
+      partyType: '가족',
+      num: 1,
+      transportationList: [],
     }
   },
   computed: {
-    optionTag() {
-      const party = this.radio
+    partyTag() {
+      const party = this.partyType
       return party
+    },
+    transportationTag() {
+      const transportation = this.transportationList
+      return transportation
+    }
+  },
+  methods: {
+    handleClose(tag) {
+      this.transportationList.splice(this.transportationList.indexOf(tag), 1)
     }
   }
 }
@@ -78,6 +107,11 @@ export default {
   margin-right: 20px;
 }
 .party-type .demonstration {
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-right: 20px;
+}
+.member-num .demonstration {
   color: var(--el-text-color-secondary);
   font-size: 14px;
   margin-right: 20px;
