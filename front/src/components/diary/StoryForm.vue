90ec125491c7fbe86c2a765c24a2586c648e3dc9
junghyun1009 <junghyunk1009@gmail.com>
2022-07-23 05:02:17 +0900

    [#S07P12A506-63][FE] feat: add story form

---
+++ front/src/components/diary/StoryForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <span>상세 장소</span>
+      <el-input v-model="newStory.place" class="w-50 m-2" placeholder="상세 장소를 입력하세요." />
+    </div>
+    <div>
+      <el-input v-model="newStory.content" maxlength="500"
+      placeholder="내용을 입력해주세요." show-word-limit type="textarea"/>
+    </div>
+    <div>
+      <span>별점</span>
+      <el-rate v-model="newStory.rate" allow-half />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoryForm',
+  props: {
+    storyForm: Object
+  },
+  data() {
+    return {
+      newStory: {
+        place: this.storyForm.place,
+        photo: this.storyForm.photp,
+        content: this.storyForm.content,
+        rate: this.storyForm.rate
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
