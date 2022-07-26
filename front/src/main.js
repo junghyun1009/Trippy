84b1d825b2a3fcc8002ce3a1d170db4fa2a80147
junghyun1009 <junghyunk1009@gmail.com>
2022-07-26 22:26:45 +0900

    [#S07P12A506-86][FE] chore: install google map

--- front/src/main.js
+++ front/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as VueGoogleMaps from "vue3-google-map"
 
 
 createApp(App).use(router).use(store).use(router).use(store).use(ElementPlus).mount('#app')
