c7b9fe326445264441076c0437603aabce10a59f
윤여빈 <tommy7899@naver.com>
2022-07-28 10:35:08 +0900

    [#S07P12A506-116][BE] feat: add RepositoriesTest

--- back/Trippy/src/main/java/com/ssafy/trippy/Repository/FollowRepository.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Repository/FollowRepository.java
@@ -1,10 +1,15 @@
 package com.ssafy.trippy.Repository;
 
-import com.ssafy.trippy.Entity.Badge;
 import com.ssafy.trippy.Entity.Follow;
+import com.ssafy.trippy.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findAllByFollowing(Member member);
+    List<Follow> findAllByFollower(Member member);
+
 }
--- back/Trippy/src/main/java/com/ssafy/trippy/Repository/TransportRepository.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Repository/TransportRepository.java
@@ -1,6 +1,5 @@
 package com.ssafy.trippy.Repository;
 
-import com.ssafy.trippy.Entity.PostTransport;
 import com.ssafy.trippy.Entity.Transport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
