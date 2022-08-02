5b4197385b93dc510e222ca8bee89d1be98a1475
junghyun1009 <junghyunk1009@gmail.com>
2022-08-02 14:21:57 +0900

    [#S07P12A506-143][FE] feat: add drawer in navbar

--- front/src/components/common/NavBar.vue
+++ front/src/components/common/NavBar.vue
@@ -1,18 +1,40 @@
 <template>
   <div>
-  <el-menu
-    :default-active="activeIndex"
-    active-text-color="#ffd04b"
-    class="el-menu-demo"
-    mode="horizontal"
-    :ellipsis="false"
-    @select="handleSelect"
-  >
-    <el-menu-item index="0"><span class="trippyLogo" @click="$router.push('/')">TRIPPY</span></el-menu-item>
-    <div class="flex-grow" />
-    <el-menu-item index="1">Hamburger</el-menu-item>
-  </el-menu>
+    <div>
+      <el-menu
+        :default-active="activeIndex"
+        active-text-color="#ffd04b"
+        class="el-menu-demo"
+        mode="horizontal"
+        :ellipsis="false"
+        @select="handleSelect"
+      >
+        <el-menu-item index="0"><span class="trippyLogo" @click="$router.push('/')">TRIPPY</span></el-menu-item>
+        <div class="flex-grow" />
+        <el-menu-item index="1">
+          <p @click="visible = true">
+            Hamburger
+          </p>
+          <el-drawer v-model="visible" :show-close="true" :modal="false" size="70%">
+            <!-- 로그인한 유저 -->
+            <p>프로필 사진</p>
+            <p>'유저 이름'의 로그</p>
+            <router-link :to="{ name: 'profileEdit' }">프로필 수정</router-link>
+            <span> | 로그아웃</span>
+            <hr>
+            <router-link :to="{ name: 'diaryCreate' }">다이어리 작성</router-link>
+            <br>
+            <router-link :to="{ name: 'community' }">동행 구하기</router-link>
+            <hr>
+            <!-- 로그인 안 한 유저 -->
+            <router-link :to="{ name: 'login' }">로그인</router-link>
+            <br>
+            <router-link :to="{ name: 'signUp' }">회원가입</router-link>
 
+          </el-drawer>
+        </el-menu-item>
+      </el-menu>
+    </div>
   </div>
 </template>
 
@@ -22,6 +44,11 @@ import { ref } from 'vue'
 
 export default {
   name: "NavBar",
+  data () {
+    return {
+      visible: false
+    }
+  },
 	methods: {
 		handleSelect(key, keyPath) {
 			var activeIndex = ref('1');
@@ -36,7 +63,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap');
 
 .trippyLogo {
