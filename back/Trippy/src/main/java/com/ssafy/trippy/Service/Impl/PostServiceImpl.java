346f98845168c63ec194551351608e93aecb86d5
윤여빈 <tommy7899@naver.com>
2022-08-07 13:58:23 +0900

    [#S07P12A506-202] feat: fix Controller

--- back/Trippy/src/main/java/com/ssafy/trippy/Service/Impl/PostServiceImpl.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Service/Impl/PostServiceImpl.java
@@ -1,17 +1,11 @@
-package com.ssafy.trippy.Service.Impl;
+package com.ssafy.trippy.Service.Impl;//package com.ssafy.trippy.Service.Impl;
 
 import com.ssafy.trippy.Domain.*;
 import com.ssafy.trippy.Dto.Request.RequestPostDto;
-import com.ssafy.trippy.Dto.Request.RequestRouteDto;
 import com.ssafy.trippy.Dto.Response.ResponsePostDto;
-import com.ssafy.trippy.Dto.Update.UpdatePostDto;
 import com.ssafy.trippy.Repository.*;
 import com.ssafy.trippy.Service.PostService;
-import com.ssafy.trippy.Service.PostTransportService;
-import com.ssafy.trippy.Service.TransportService;
-import com.sun.mail.iap.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,15 +16,14 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
     private final DetailLocationRepository detailLocationRepository;
     private final PostTransportRepository postTransportRepository;
-
-    private final TransportRepository transportRepository;
-
+    private final LocationRepository locationRepository;
     private final PostCommentRepository postCommentRepository;
     private final RouteRepository routeRepository;
 
@@ -40,7 +33,6 @@ public class PostServiceImpl implements PostService {
 
 
     // 모든 post찾기
-    @Transactional(readOnly = true)
     @Override
     public List<ResponsePostDto> findAll() {
         List<Post> all = postRepository.findAll();
@@ -55,7 +47,6 @@ public class PostServiceImpl implements PostService {
     }
 
     // 유저(본인)가 작성한 게시글 조회
-    @Transactional(readOnly = true)
     @Override
     public List<ResponsePostDto> findAllByMember(Member member) {
         List<Post> posts = postRepository.findAllByMember(member);
@@ -74,6 +65,18 @@ public class PostServiceImpl implements PostService {
         Member member = memberRepository.findById(requestPostDto.getMember_id()).get();
         requestPostDto.setMember_id(member.getId());
         Post post = postRepository.save(requestPostDto.toEntity());
+
+        // 요청들어온 post 내의 countryName, cityName으로 location 저장
+        Optional<Location> location = locationRepository.findByCityNameAndCountryName(requestPostDto.getCityName(), requestPostDto.getCountryName());
+
+        // location의 Id값을 detailLocation에 저장
+        if (location.isPresent()) {
+            requestPostDto.setLocationId(location.get().getId());
+        } else {
+            Long locationId = locationRepository.save(Location.builder().countryName(requestPostDto.getCountryName())
+                    .cityName(requestPostDto.getCityName()).build()).getId();
+            requestPostDto.setLocationId(locationId);
+        }
 
         for (DetailLocation detailLocation : requestPostDto.toEntity().getDetailLocations().stream().collect(Collectors.toList())) {
             detailLocation.setPost(post);
@@ -120,6 +123,7 @@ public class PostServiceImpl implements PostService {
 
         List<DetailLocation> detailLocationList = post.getDetailLocations();
 
+
         // PostTransport 테이블에도 수정된 값 넣어주기
         for (PostTransport postTransport : requestPostDto.toEntity().getPostTransports().stream().collect(Collectors.toList())) {
             if (postTransport.getTransport().getName().equals("버스")) {
@@ -130,13 +134,21 @@ public class PostServiceImpl implements PostService {
                 postTransport.getTransport().builder().id(taxiId).build();
             }
         }
-
         // detailLocation 테이블에도 수정된 값 넣어주기
+        // location의 Id값을 detailLocation에 저장
+        Optional<Location> location = locationRepository.findByCityNameAndCountryName(requestPostDto.getCityName(), requestPostDto.getCountryName());
+        if (location.isPresent()) {
+            requestPostDto.setLocationId(location.get().getId());
+        } else {
+            Long locationId = locationRepository.save(Location.builder().countryName(requestPostDto.getCountryName())
+                    .cityName(requestPostDto.getCityName()).build()).getId();
+            requestPostDto.setLocationId(locationId);
+        }
+
         List<DetailLocation> detailLocation = requestPostDto.toEntity().getDetailLocations().stream().collect(Collectors.toList());
         List<DetailLocation> detailLocations = new ArrayList<>();
         for (DetailLocation detail : detailLocation) {
             detail.setPost(post);
-            ;
             detailLocations.add(detail);
         }
 
