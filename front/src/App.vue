5f48f1e50f42380630d1caad3197cab28cd72b86
정유송 <yoosong0713@naver.com>
2022-08-08 13:27:40 +0900

    [#S07P12A506-228][FE] feat: modify navbar structure

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <nav-bar></nav-bar>
+    <the-header></the-header>
     <router-view/>
+    <nav-bar v-if="this.$route.name !== 'login' && this.$route.name !== 'signUp' && this.$route.name !== 'signUpOption'"></nav-bar>
   </div>
 </template>
 
 <script>
+import TheHeader from '@/components/common/TheHeader.vue'
 import NavBar from '@/components/common/NavBar.vue'
-
 export default ({
   name: 'App',
   components: {
+    TheHeader,
     NavBar
+  },
+  methods: {
+    // notShowLoginJoin() {
+    //   this.$route.name !== 'login'
+    //   this.$route.name !== 'signUp'
+    // }
   }
 })
 </script>
