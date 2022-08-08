6fdd424620463ca5f5c8c75e03cbff0df76d2a78
Yoojin <0312jj@naver.com>
2022-08-08 14:11:55 +0900

    [S07P12A506-273][BE] fix: BookmarkController getId from access token

--- back/Trippy/src/main/java/com/ssafy/trippy/Controller/BookmarkController.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Controller/BookmarkController.java
@@ -3,38 +3,46 @@ package com.ssafy.trippy.Controller;
 import com.ssafy.trippy.Dto.Request.RequestBookmarkDto;
 import com.ssafy.trippy.Dto.Response.ResponseBookmarkDto;
 import com.ssafy.trippy.Service.BookmarkService;
+import com.ssafy.trippy.Service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/bookmark")
+@RequestMapping("/auth/bookmark")
 public class BookmarkController {
 
+    private final MemberService memberService;
     private final BookmarkService bookmarkService;
     private static final String SUCCESS = "success";
 
-    @PostMapping("/api")
-    public ResponseEntity<?> saveBookmark(@RequestBody RequestBookmarkDto requestBookmarkDto){
+    @PostMapping("/{community_post_id}")
+    public ResponseEntity<?> saveBookmark(HttpServletRequest request, @PathVariable("community_post_id") Long communityPostId){
+        Long memberId = memberService.getIdByToken(request.getHeader("X-AUTH-TOKEN"));
+        RequestBookmarkDto requestBookmarkDto = new RequestBookmarkDto(communityPostId, memberId);
         bookmarkService.saveBookmark(requestBookmarkDto);
         return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
     }
 
-    @GetMapping("/api/{member_id}")
-    public ResponseEntity<?> getBookmarks(@PathVariable("member_id") Long memberId){
+    @GetMapping
+    public ResponseEntity<?> getBookmarks(HttpServletRequest request){
+        Long memberId = memberService.getIdByToken(request.getHeader("X-AUTH-TOKEN"));
         List<ResponseBookmarkDto> responseBookmarkDtos = bookmarkService.getBookmarks(memberId);
         return new ResponseEntity<>(responseBookmarkDtos, HttpStatus.OK);
     }
 
-    @DeleteMapping("/api")
-    public ResponseEntity<?> removeBookmark(@RequestBody RequestBookmarkDto requestBookmarkDto){
+    @DeleteMapping("/{community_post_id}")
+    public ResponseEntity<?> removeBookmark(HttpServletRequest request, @PathVariable("community_post_id") Long communityPostId){
+        Long memberId = memberService.getIdByToken(request.getHeader("X-AUTH-TOKEN"));
+        RequestBookmarkDto requestBookmarkDto = new RequestBookmarkDto(communityPostId,memberId);
         bookmarkService.deleteBookmark(requestBookmarkDto);
         return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
     }
