03e3251a36fd89b1e96bc2ae8dbeb6c2fbe7528a
junghyun1009 <junghyunk1009@gmail.com>
2022-08-10 14:00:05 +0900

    [#S07P12A506-253][FE] feat: add reload code in App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,6 +9,7 @@
 <script>
 import TheHeader from '@/components/common/TheHeader.vue'
 import NavBar from '@/components/common/NavBar.vue'
+import { mapActions } from 'vuex';
 export default ({
   name: 'App',
   components: {
@@ -20,11 +21,28 @@ export default ({
     //   this.$route.name !== 'login'
     //   this.$route.name !== 'signUp'
     // }
-  }
+    ...mapActions(['reissueToken'])
+  },
+  mounted() {
+    if (localStorage.getItem('reloaded')) {
+        localStorage.removeItem('reloaded');
+    } else {
+        localStorage.setItem('reloaded', '1');
+        location.reload();
+    }
+    this.reissueToken()
+  },
 })
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@300;500&display=swap');
+
+  * { 
+      font-family: 'IBM Plex Sans KR', sans-serif;
+      font-weight: 100;
+    } 
+
 :root {
   --el-color-primary: #F16B51 !important;
 }
