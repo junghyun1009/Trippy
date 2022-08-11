d07d16fd0394983a25342564e092cb50e664de84
uussong <yoosong0713@gmail.com>
2022-08-11 09:47:48 +0900

    [#S07P12A506-313][FE] feat: set setting structure

--- front/src/views/account/SettingView.vue
+++ front/src/views/account/SettingView.vue
@@ -1,22 +1,33 @@
 <template>
-  <div>
-    <p @click="goProfileEdit">
-      프로필 수정
-      <span class="material-symbols-outlined icon">arrow_forward_ios</span>
-    </p>
-    
-    <p @click="goPasswordChange">
-      비밀번호 변경
-      <span class="material-symbols-outlined icon">arrow_forward_ios</span>
-    </p>
-    <p @click="logout">
-      로그아웃
-      <span class="material-symbols-outlined icon">arrow_forward_ios</span>
-    </p>
-    <p @click="open(), deleteAccount()">
-      회원 탈퇴
-      <span class="material-symbols-outlined icon">arrow_forward_ios</span>
-    </p>
+  <div class="container">
+    <div class="menu">
+      <p @click="goProfileEdit">
+        <span class="material-symbols-outlined icon">person</span>
+        프로필 수정
+      </p>
+      <span class="material-symbols-outlined arrow">arrow_forward_ios</span>
+    </div>
+    <div class="menu">
+      <p @click="goPasswordChange">
+        <span class="material-symbols-outlined icon">key</span>
+        비밀번호 변경
+      </p>
+      <span class="material-symbols-outlined arrow">arrow_forward_ios</span>
+    </div>
+    <div class="menu">
+      <p @click="logout">
+        <span class="material-symbols-outlined icon">logout</span>
+        로그아웃
+      </p>
+      <span class="material-symbols-outlined arrow">arrow_forward_ios</span>
+    </div>
+    <div class="menu">
+      <p @click="open(), deleteAccount()">
+        <span class="material-symbols-outlined icon">delete_forever</span>
+        회원 탈퇴
+      </p>
+      <span class="material-symbols-outlined arrow">arrow_forward_ios</span>
+    </div>
   </div>
 </template>
 
@@ -64,19 +75,29 @@ export default {
 </script>
 
 <style scoped>
-* {
-  padding: 1rem;
+
+.container {
+  padding: 2rem 1rem;
 }
 
-p {
-  margin: 0.8rem 0;
+.menu {
+  margin: 1rem 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
+p {
+  display: flex;
+  align-items: center;
+}
+
 .icon {
+  margin-right: 0.6rem;
+}
+
+.arrow {
   font-size: 1rem;
-  padding: 0;
+  color: #F16B51;
 }
 </style>
