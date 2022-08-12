a6b964740f236148d13abdd69fe7f81e15e942c9
younggilPark <ygpark1230@gmail.com>
2022-08-12 11:57:45 +0900

    [#S07P12A506-247][BE] chore: add class for method error

--- back/Trippy/src/main/java/com/ssafy/trippy/Controller/CommunityPostController.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Controller/CommunityPostController.java
@@ -18,7 +18,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class CommunityPostController {
 
     private final CommunityPostService communityPostService;
