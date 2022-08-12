eab48dd75419a8b858e2201563134b22943330ba
윤여빈 <tommy7899@naver.com>
2022-08-12 17:23:52 +0900

    [#S07P12A506-161] fix: CommunityPostController에서 ResponseCommunityPostDto에 id값 추가

--- back/Trippy/src/main/java/com/ssafy/trippy/Controller/CommunityPostController.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Controller/CommunityPostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
