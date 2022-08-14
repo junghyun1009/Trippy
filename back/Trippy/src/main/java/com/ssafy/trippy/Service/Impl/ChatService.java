8152797869ca62ddc3eda1d8101b76bf96c6fb2c
윤여빈 <tommy7899@naver.com>
2022-08-14 20:18:35 +0900

    [#S07P12A506-323] fix: 마지막 chat controller, dto, service 도전

--- back/Trippy/src/main/java/com/ssafy/trippy/Service/Impl/ChatService.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Service/Impl/ChatService.java
@@ -1,45 +1,81 @@
 package com.ssafy.trippy.Service.Impl;
 
-import com.ssafy.trippy.Dto.ChatDto.ChatMessage;
-import com.ssafy.trippy.Repository.ChatRoomRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.stereotype.Service;
 
+import com.ssafy.trippy.Dto.ChatDto.ChatMessage;
+import com.ssafy.trippy.Dto.ChatDto.ChatRoom;
+import com.ssafy.trippy.Repository.ChatRoomRepository;
+import com.ssafy.trippy.Service.Subscriber.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
-@Service
+@Repository
 public class ChatService {
-
-
-    private final ChannelTopic channelTopic;
-    private final RedisTemplate redisTemplate;
+    // 채팅방(topic)에 발행되는 메시지를 처리할 Listner
+    private final RedisMessageListenerContainer redisMessageListener;
     private final ChatRoomRepository chatRoomRepository;
+    // 구독 처리 서비스
+    private final RedisSubscriber redisSubscriber;
+    // Redis
+    private static final String CHAT_ROOMS = "CHAT_ROOM";
+    private final RedisTemplate<String, Object> redisTemplate;
+    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+    // 채팅방의 대화 메시지를 발행하기 위한 redis topic 정보. 서버별로 채팅방에 매치되는 topic정보를 Map에 넣어 roomId로 찾을수 있도록 한다.
+    private Map<String, ChannelTopic> topics;
 
-    /**
-     * destination정보에서 roomId 추출
-     */
-    public String getRoomId(String destination) {
-        int lastIndex = destination.lastIndexOf('/');
-        if (lastIndex != -1)
-            return destination.substring(lastIndex + 1);
-        else
-            return "";
+    @PostConstruct
+    private void init() {
+        opsHashChatRoom = redisTemplate.opsForHash();
+        topics = new HashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        return opsHashChatRoom.values(CHAT_ROOMS);
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return opsHashChatRoom.get(CHAT_ROOMS, id);
     }
 
     /**
-     * 채팅방에 메시지 발송
+     * 채팅방 생성 : 서버간 채팅방 공유를 위해 redis hash에 저장한다.
      */
-    public void sendChatMessage(ChatMessage chatMessage) {
-        chatMessage.setUserCount(chatRoomRepository.getUserCount(chatMessage.getRoomId()));
-        if (ChatMessage.MessageType.ENTER.equals(chatMessage.getType())) {
-            chatMessage.setMessage(chatMessage.getSender() + "님이 방에 입장했습니다.");
-            chatMessage.setSender("[알림]");
-        } else if (ChatMessage.MessageType.QUIT.equals(chatMessage.getType())) {
-            chatMessage.setMessage(chatMessage.getSender() + "님이 방에서 나갔습니다.");
-            chatMessage.setSender("[알림]");
-        }
-        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+    /**
+     * 채팅방 삭제
+     */
+    public void deleteById(ChatRoom chatRoom){
+        chatRoomRepository.delete(chatRoom);
+    }
+
+    /**
+     * 채팅방 입장 : redis에 topic을 만들고 pub/sub 통신을 하기 위해 리스너를 설정한다.
+     */
+    public void enterChatRoom(String roomId) {
+        ChannelTopic topic = topics.get(roomId);
+        if (topic == null)
+            topic = new ChannelTopic(roomId);
+        redisMessageListener.addMessageListener(redisSubscriber, topic);
+        topics.put(roomId, topic);
+    }
+
+    public ChannelTopic getTopic(String roomId) {
+        return topics.get(roomId);
     }
 }
