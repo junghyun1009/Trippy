9d5278553827f2dcefdd790385a97cbc154b4c5b
홍유진 <0312jj@naver.com>
2022-08-16 02:37:21 +0900

    [S07P12A506-270][BE] feature : add getBadges

--- back/Trippy/src/main/java/com/ssafy/trippy/Service/BadgeService.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Service/BadgeService.java
@@ -4,6 +4,10 @@ package com.ssafy.trippy.Service;
 import com.ssafy.trippy.Domain.Badge;
 import com.ssafy.trippy.Dto.Response.ResponseBadgeDto;
 
+import java.util.List;
+
 public interface BadgeService {
     ResponseBadgeDto saveBadge(Long badgeId,Long memberId);
+
+    List<ResponseBadgeDto> getBadgesByMemberId(Long memberId);
 }
