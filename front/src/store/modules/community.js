ee880bd743a2ae6c52ad4cc5d8a9ee9ce50c7d4d
junghyun1009 <junghyunk1009@gmail.com>
2022-08-24 03:16:58 +0900

    feat: search in diary and community page

--- front/src/store/modules/community.js
+++ front/src/store/modules/community.js
@@ -6,6 +6,7 @@ export default ({
   state: {
     posts: [],
     post: {},
+    searchCommunities: []
     // isBookmark: false,
   },
   getters: {
@@ -15,6 +16,7 @@ export default ({
     isPostAuthor: (state, getters) => {
       return state.post.name === getters.currentUser.name
     },
+    searchCommunities: state => state.searchCommunities
     // temp: state => state.temp,
     
   },
@@ -22,6 +24,7 @@ export default ({
     SET_POSTS: (state, posts) => state.posts = posts,
     SET_POST: (state, post) => state.post = post,
     SET_POST_BOOKMARK: (state, bookmark) => (state.post.bookmark = bookmark),
+    SET_SEARCH_COMMUNITIES: (state, result) => (state.searchCommunities = result) 
   },
   actions: {
     // 첫번째 게시글인가요?
@@ -181,6 +184,52 @@ export default ({
         // }
       })
       .catch(err => console.err(err.response))
+    },
+
+    // 동행 검색 - 나라
+    searchCommunity({ dispatch }, searchInput) {
+      console.log(searchInput)
+      const country = searchInput.region
+      axios({
+        url: `https://i7a506.p.ssafy.io/api/search/communityPost?countryName=${country}`,
+        // url: `https://i7a506.p.ssafy.io/api/search?title=${title}`,
+        // url: 'https://i7a506.p.ssafy.io/api/search' + '?' + `${title}` + '?' + `${company}` + '?' + `${transport}`,
+        method: 'get',
+        params: country
+      })
+      .then( res => {
+        const payload = {
+          result: res.data,
+          query: {
+            region: country
+          }
+        }
+        dispatch('searchCommunityCity', payload)
+        }
+      )
+    },
+
+    // 동행 검색 - 도시
+    searchCommunityCity({ commit }, searchInput) {
+      const city = searchInput.query.region
+      axios({
+        url: `https://i7a506.p.ssafy.io/api/search/communityPost?cityName=${city}`,
+        method: 'get',
+        params: city
+      })
+      .then( res => {
+        const payload = {
+          result: [...searchInput.result, ...res.data],
+          query: searchInput
+        }
+        console.log(payload.result)
+        var searchResult = payload.result
+        if ( searchResult.length === 0 ){
+          console.log('검색결과가 없습니다')
+        }
+        commit('SET_SEARCH_COMMUNITIES', payload.result),
+        console.log('search completed')
+      })
     },
 
   },
