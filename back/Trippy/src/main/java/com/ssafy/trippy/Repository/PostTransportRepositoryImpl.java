6099cf3c10253043b3bb92faf23556ac3b00cf9a
윤여빈 <tommy7899@naver.com>
2022-08-23 22:35:56 +0900

    [#S07P12A506-415] add: 검색할 때 나라이름, 도시이름 포함해서 검색, 동행찾기 검색 api 추가

--- back/Trippy/src/main/java/com/ssafy/trippy/Repository/PostTransportRepositoryImpl.java
+++ back/Trippy/src/main/java/com/ssafy/trippy/Repository/PostTransportRepositoryImpl.java
@@ -1,10 +1,8 @@
 package com.ssafy.trippy.Repository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.trippy.Domain.Post;
-import com.ssafy.trippy.Domain.PostTransport;
-import com.ssafy.trippy.Domain.QPost;
-import com.ssafy.trippy.Domain.QPostTransport;
+import com.ssafy.trippy.Domain.*;
+import com.ssafy.trippy.Dto.Response.ResponseCommunityPostDto;
 import com.ssafy.trippy.Dto.Response.ResponsePostDto;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
@@ -23,10 +21,11 @@ public class PostTransportRepositoryImpl implements PostTransportRepositoryCusto
 
 
     @Override
-    public List<ResponsePostDto> findAllBySearch(String title, int company, Long transportId) {
+    public List<ResponsePostDto> findAllBySearch(String title, int company, Long transportId,String countryName,String cityName) {
         Set<Long> postId = new HashSet<>();
         QPostTransport transport = QPostTransport.postTransport;
         QPost post = QPost.post;
+        QLocation location = QLocation.location;
         BooleanBuilder builder = new BooleanBuilder();
         if(company>0 ){
             builder.and(post.company.eq(company));
@@ -37,9 +36,16 @@ public class PostTransportRepositoryImpl implements PostTransportRepositoryCusto
         if(transportId>0){
             builder.and(transport.transport.id.eq(transportId));
         }
+        if(!StringUtils.isEmpty(countryName)){
+            builder.and(location.countryName.containsIgnoreCase(countryName));
+        }
+        if(!StringUtils.isEmpty(cityName)){
+            builder.and(location.cityName.containsIgnoreCase(cityName));
+        }
 
         List<PostTransport> postList = jpaQueryFactory.selectFrom(transport)
                 .join(transport.post, post)
+                .join(post.location,location)
                 .where(builder)
                 .fetch();
         List<ResponsePostDto> responsePostDtos = new ArrayList<>();
@@ -53,6 +59,30 @@ public class PostTransportRepositoryImpl implements PostTransportRepositoryCusto
             responsePostDtos.add(dto);
         }
         return responsePostDtos;
+    }
+
+    @Override
+    public List<ResponseCommunityPostDto> findAllByCommunitySearch(String countryName, String cityName) {
+        QLocation location = QLocation.location;
+        QCommunityPost communityPost = QCommunityPost.communityPost;
+        BooleanBuilder builder = new BooleanBuilder();
+        if(!StringUtils.isEmpty(countryName)){
+            builder.and(location.countryName.containsIgnoreCase(countryName));
+        }
+        if(!StringUtils.isEmpty(cityName)){
+            builder.and(location.cityName.containsIgnoreCase(cityName));
+        }
+        List<CommunityPost> postList = jpaQueryFactory.selectFrom(communityPost)
+                .join(communityPost.location,location)
+                .where(builder)
+                .fetch();
+
+        List<ResponseCommunityPostDto> communityPostDtos = new ArrayList<>();
+        for (CommunityPost post : postList) {
+            ResponseCommunityPostDto communityPostDto = new ResponseCommunityPostDto(post);
+            communityPostDtos.add(communityPostDto);
+        }
+        return communityPostDtos;
     }
 
 
